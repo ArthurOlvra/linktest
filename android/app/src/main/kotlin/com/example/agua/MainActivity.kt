20b1a9d3f042a16c172542854518863cd79c95d3
ArthurOlvra <arthurphbfull12@gmail.com>
2021-07-29 16:08:13 -0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/agua/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.agua
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:segundo_teste/scaffold.dart';
+import 'package:segundo_teste/botaoprox';
+
+void main() {
+  runApp(
+    MaterialApp(debugShowCheckedModeBanner: false, home: Agua()),
+  );
+}
